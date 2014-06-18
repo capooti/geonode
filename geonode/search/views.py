8988b10f3c551d9a8750ec5f0b4bc0b896a16f1f
CORTI Paolo <paolo.corti@wfp.org>
2014-06-18 17:49:26 +0200

    Implemented search for wfpdocuments.

--- geonode/search/views.py
+++ geonode/search/views.py
@@ -44,6 +44,7 @@ import cPickle as pickle
 import operator
 import logging
 import zlib
+import collections
 
 logger = logging.getLogger(__name__)
 
@@ -62,21 +63,45 @@ _viewer_config = _create_viewer_config()
 def search_page(request, template='search/search.html', **kw):
     initial_query = request.REQUEST.get('q','')
     results, facets, query = search_api(request, format='html', **kw)
-    tags = {}
-
+    
+    # get the wfp categories and their count (just for wfpdocuments!)
+    categories = {}
+    for item in results:
+        if hasattr(item.o, 'wfpdocument'):
+            for category_item in item.o.wfpdocument.categories.all():
+                categories[category_item.name] = categories.get(category_item.name,{})
+                categories[category_item.name]['slug'] = category_item.name
+                categories[category_item.name]['name'] = category_item.name
+                categories[category_item.name]['count'] = categories[category_item.name].get('count',0) + 1
+    categories = collections.OrderedDict(sorted(categories.items()))
+    
+    # get the regions and their count
+    regions = {}
+    for item in results:
+        for region_item in item.o.regions.all():
+            regions[region_item.name] = regions.get(region_item.name,{})
+            regions[region_item.name]['slug'] = region_item.name
+            regions[region_item.name]['name'] = region_item.name
+            regions[region_item.name]['count'] = regions[region_item.name].get('count',0) + 1
+    regions = collections.OrderedDict(sorted(regions.items()))
+    
     # get the keywords and their count
+    tags = {}
     for item in results:
         for tagged_item in item.o.tagged_items.all():
-                tags[tagged_item.tag.slug] = tags.get(tagged_item.tag.slug,{})
-                tags[tagged_item.tag.slug]['slug'] = tagged_item.tag.slug
-                tags[tagged_item.tag.slug]['name'] = tagged_item.tag.name
-                tags[tagged_item.tag.slug]['count'] = tags[tagged_item.tag.slug].get('count',0) + 1
-
+            tags[tagged_item.tag.slug] = tags.get(tagged_item.tag.slug,{})
+            tags[tagged_item.tag.slug]['slug'] = tagged_item.tag.slug
+            tags[tagged_item.tag.slug]['name'] = tagged_item.tag.name
+            tags[tagged_item.tag.slug]['count'] = tags[tagged_item.tag.slug].get('count',0) + 1
+            
     total = 0
+    
     for val in facets.values(): total+=val
     total -= facets['raster'] + facets['vector'] + facets['remote']
+    
     return render_to_response(template, RequestContext(request, {'object_list': results, 'total': total, 
-        'facets': facets, 'query': json.dumps(query.get_query_response()), 'tags': tags,
+        'facets': facets, 'query': json.dumps(query.get_query_response()), 
+        'tags': tags, 'categories': categories, 'regions': regions,
         'initial_query': initial_query}))
 
 def advanced_search(request, **kw):
