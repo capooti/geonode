8c9b659984715b070b847140e4c476bb5581b53e
capooti <pcorti@gmail.com>
2017-09-07 16:55:50 -0400

    Added createlayer in GEONODE_CONTRIB_APPS (commented out)

--- geonode/settings.py
+++ geonode/settings.py
@@ -293,7 +293,7 @@ GEONODE_CONTRIB_APPS = (
     # 'geonode.contrib.geosites',
     # 'geonode.contrib.nlp',
     # 'geonode.contrib.slack',
-    #'geonode.contrib.createlayer',
+    'geonode.contrib.createlayer',
     'geonode.contrib.metadataxsl',
     'geonode.contrib.api_basemaps',
 )
